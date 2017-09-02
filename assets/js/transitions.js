8bd6108613b6b6aaeb21144c1a69e8b28d7485ab
Nandun Bandara <ntbandara3@hotmail.com>
2017-09-03 01:47:41 +0530

    back to top button created

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -1,3 +1,27 @@
 /**
  * Created by nandunb on 9/2/17.
  */
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+    $('#btt').hide();
+});
+
+
+$(document).scroll(function(){
+    var y = $(this).scrollTop();
+    if(y > 300){
+        $('#btt').fadeIn();
+    }else{
+        $('#btt').fadeOut();
+    }
+});
