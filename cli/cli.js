db7e86cc8261a10ab78eae8117b71b89e1e09ef5
Nandun Bandara <ntbandara3@hotmail.com>
2021-10-02 06:59:17 +0530

    feat: cli help and get commands

--- cli/cli.js
+++ cli/cli.js
@@ -13,14 +13,51 @@ const handleInput = (event) => {
         const lastInput = value.split('> ').pop();
         console.log(lastInput);
 
-        cli.value = value + '\n> ';
+        let out;
+        switch(lastInput) {
+            case '':
+                event.preventDefault();
+                return;
+            case 'help':
+                out = getHelpText();
+                break;
+            case 'whoami':
+                out = getWhoAmI();
+                break;
+            case 'clear':
+                cli.value = '> ';
+                return;
+            case 'get.github':
+                out = 'https://github.com/nandunbandara';
+                break;
+            case 'get.twitter':
+                out = 'https://twitter.com/nandunbandaraa';
+                break
+            default:
+                out = 'command not found'
+        }
+
+        cli.value = value + '\n' + out + '\n> ';
+        cli.value = cli.value.slice(0, -1) + ' ';
     }
 
     if (event.keyCode === 8) {
-        const {value} = document.getElementById('cli');
+        const cli = document.getElementById('cli');
+        console.log(window.getSelection());
+        const {value} = cli;
         const tokens = value.split('');
         if (tokens.slice(tokens.length - 2, tokens.length).join('') === '> ') {
             event.preventDefault();
+        } else {
+            const selection = window.getSelection();
+            if (selection.type === 'Range') {
+                event.preventDefault();
+            }
         }
     }
 }
+
+
+const getWhoAmI = () => `Hello! I am Nandun Bandara. Welcome to my personal website.`
+const getHelpText = () => `get.github\t--\tReturns github profile\n` +
+    `get.twitter\t--\tReturns twitter handle\n`;
